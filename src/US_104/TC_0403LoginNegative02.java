9fee35a003a064a380bf4af27250a802f92c656e
bughunter-tugba <tugbakilic3302@gmail.com>
2025-03-17 13:08:34 +0300

    TC_0403LoginNegative02 test otomasyon kodlari yazildi ve test edildi.

--- src/US_104/TC_0403LoginNegative02.java
+++ src/US_104/TC_0403LoginNegative02.java
@@ -1,7 +1,11 @@
 package US_104;
 
 import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class TC_0403LoginNegative02 extends BaseDriver {
     /**Bu Test Senaryosu, geçersiz bir e-posta adresiyle oturum açma girişiminin reddedildiğini test etmek için hazırlanmıştır.
@@ -10,9 +14,35 @@ public class TC_0403LoginNegative02 extends BaseDriver {
     @Test
     public void TC_0403LoginTest() {
         driver.navigate().to("https://www.akakce.com/");
+        Assert.assertEquals("Web sitesine erisim saglanamadi.", "https://www.akakce.com/", driver.getCurrentUrl());
+        System.out.println("Anasayfaya başarılı bir şekilde ulaşıldı.");
+        MyFunc.sleep(2);
 
+        WebElement login = driver.findElement(By.cssSelector("div[id='H_rl_v8'] :nth-child(2)"));
+        login.click();
+        System.out.println("Giriş yap butonuna tıklandı.");
+        MyFunc.sleep(2);
 
+        WebElement fakeEmail = driver.findElement(By.cssSelector("div[id='L_h_v8'] :nth-child(4) input"));
+        fakeEmail.sendKeys("hatali@email.com");
+        Assert.assertEquals("Hatali e-posta adresi girilemedi.", "hatali@email.com", fakeEmail.getAttribute("value"));
+        System.out.println("Gecersiz e-posta adresi girildi.");
+        MyFunc.sleep(2);
 
+        WebElement correctPassword = driver.findElement(By.cssSelector("div[id='L_h_v8'] :nth-child(6) input"));
+        correctPassword.sendKeys("Tz12345678.");
+        Assert.assertEquals("Şifre girilemedi.", "Tz12345678.", correctPassword.getAttribute("value"));
+        System.out.println("Şifre girildi.");
+        MyFunc.sleep(2);
+
+        WebElement loginButton = driver.findElement(By.cssSelector("div[id='L_h_v8'] :nth-child(10)"));
+        loginButton.click();
+        System.out.println("Giriş yap butonuna tıklandı.");
+        MyFunc.sleep(2);
+
+        WebElement errorMessage=driver.findElement(By.cssSelector("div#m-w p"));
+        Assert.assertEquals("Hatali e-posta adresi girilmesine ragmen hata mesaji gorulmedi.","Bu e-postaya kayıtlı bir hesap bulunamadı.",errorMessage.getText());
+        System.out.println("Hatali e-posta adresi girildi ve hata mesaji goruntulendi.");
 
         WaitAndClose();
     }

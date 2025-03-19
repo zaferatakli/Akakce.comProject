36ced98d5af6be52e8efa1d696caa96485819af5
zaferatakli <zatakli@gmail.com>
2025-03-19 17:47:08 +1100

    US-101 package altindaki class' larda  guncelleme ve duzeltmeler yapildi.

--- src/US_101/TC_102Account.java
+++ src/US_101/TC_102Account.java
@@ -6,8 +6,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 public class TC_102Account extends BaseDriver {
-    /*
-    Bu senaryoda soyadı kısmı boş bırakılarak hata mesajı alınması sağlanmıştır.
+    /**
+     * Bu senaryoda soyadı kısmı boş bırakılarak hata mesajı alınması sağlanmıştır.
+     * In this scenario, the surname section was left blank, resulting in an error message.
      */
     @Test
     public void TC_0102AccountTest() {
@@ -15,6 +16,7 @@ public class TC_102Account extends BaseDriver {
         MyFunc.sleep(2);
         Assert.assertEquals("Yanlış sayfaya girldi", "https://www.akakce.com/", driver.getCurrentUrl());
         System.out.println("Akakçe sayfasına girildi");
+        MyFunc.sleep(2);
 
         WebElement signUp = driver.findElement(By.cssSelector("[id='H_st_v8'] + div >:nth-child(1)"));
         Assert.assertEquals("Yanlış sayfaya girldi", "https://www.akakce.com/", driver.getCurrentUrl());
@@ -27,84 +29,98 @@ public class TC_102Account extends BaseDriver {
         MyFunc.sleep(2);
         Assert.assertEquals("Behlul", name.getAttribute("value"));
         System.out.println("Ad girişi başarılı");
+        MyFunc.sleep(2);
 
         WebElement surname = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(7) >input"));
         surname.sendKeys(" ");
         MyFunc.sleep(2);
         Assert.assertEquals(" ", surname.getAttribute("value"));
         System.out.println("Soyad kısmı boş bırakıldı");
+        MyFunc.sleep(2);
 
         WebElement email = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(9) >input"));
         email.sendKeys("fadoda7438@dmener.com");
         MyFunc.sleep(2);
         Assert.assertEquals("fadoda7438@dmener.com", email.getAttribute("value"));
         System.out.println("Email girişi başarılı");
+        MyFunc.sleep(2);
 
         WebElement reEmail = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(11) >input"));
         reEmail.sendKeys("fadoda7438@dmener.com");
         MyFunc.sleep(2);
         Assert.assertEquals("fadoda7438@dmener.com", reEmail.getAttribute("value"));
         System.out.println("Tekrar Email girişi başarılı");
+        MyFunc.sleep(2);
 
         WebElement password = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(13) >input"));
         password.sendKeys("Bugfathers04");
         MyFunc.sleep(2);
         Assert.assertEquals("Bugfathers04", password.getAttribute("value"));
         System.out.println("Şifre girişi başarılı");
+        MyFunc.sleep(2);
 
         WebElement rePassword = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(15) >input"));
         rePassword.sendKeys("Bugfathers04");
         MyFunc.sleep(2);
         Assert.assertEquals("Bugfathers04", rePassword.getAttribute("value"));
         System.out.println("Tekrar Şifre girişi başarılı");
+        MyFunc.sleep(2);
 
         WebElement gender = driver.findElement(By.cssSelector("[id='FrmRnuS'] span ~ span label+label >input"));
         gender.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", gender.isSelected());
         System.out.println("Erkek seçildi");
+        MyFunc.sleep(2);
 
         WebElement city = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(18) select >:nth-child(2)"));
         city.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", city.isSelected());
         System.out.println("İl doğru girldi");
+        MyFunc.sleep(2);
 
         WebElement district = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(19) label + select >:nth-child(6)"));
         district.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", district.isSelected());
         System.out.println("ilçe doğru girldi");
+        MyFunc.sleep(2);
 
         WebElement day = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(23) span select >:nth-child(2)"));
         day.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", day.isSelected());
         System.out.println("Gün doğru girldi");
+        MyFunc.sleep(2);
 
         WebElement month = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(23) span + span select >:nth-child(2)"));
         month.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", month.isSelected());
         System.out.println("Ay doğru girldi");
+        MyFunc.sleep(2);
 
         WebElement year = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(23) span + span + span select >:nth-child(14)"));
         year.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", year.isSelected());
         System.out.println("Yıl doğru girldi");
+        MyFunc.sleep(2);
 
         WebElement accept = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(24) input"));
         accept.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", accept.isSelected());
         System.out.println("Sözleşme kabul et seçildi");
+        MyFunc.sleep(2);
 
         WebElement notification = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(25) input"));
         notification.click();
         MyFunc.sleep(2);
         Assert.assertTrue("Hatalı seçim", notification.isSelected());
         System.out.println("Bildirimleri kabul et seçildi");
+        MyFunc.sleep(2);
 
         WebElement submit = driver.findElement(By.cssSelector("[id='FrmRnuS'] >:nth-child(27) "));
         submit.click();

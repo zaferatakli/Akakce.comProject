8d08bd96d7a969445b4582be56d6b35aae962e70
zaferatakli <zatakli@gmail.com>
2025-03-21 12:06:30 +1100

    BaseDriver'a Random Email ve Sifre ureten metodlar eklendi.

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -3,6 +3,7 @@ package Utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
+import java.util.Random;
 
 public class BaseDriver {
     public static WebDriver driver;
@@ -16,5 +17,33 @@ public class BaseDriver {
     public static void WaitAndClose() {
         MyFunc.sleep(6);
         driver.quit();
+    }
+    public static String generateRandomEmail() {
+        String characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        String[] domains = {"gmail.com", "yahoo.com", "outlook.com", "hotmail.com"};
+        Random random = new Random();
+
+        StringBuilder username = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            username.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        String domain = domains[random.nextInt(domains.length)];
+
+        return username.toString() + "@" + domain;
+    }
+    public static String generateRandomPassword() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+        Random random = new Random();
+
+        StringBuilder password = new StringBuilder();
+        while (!password.toString().matches(".*[A-Z].*") || !password.toString().matches(".*[a-z].*") || !password.toString().matches(".*[0-9].*") || !password.toString().matches(".*[!@#$%^&*].*")) {
+            password.setLength(0);
+            for (int i = 0; i < 8; i++) {
+                password.append(characters.charAt(random.nextInt(characters.length())));
+            }
+        }
+
+        return password.toString();
     }
 }
